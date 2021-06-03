2fdc97ce4161e911b980cc33456e392c3008d129
PraneethaSompalli <85060536+PraneethaSompalli@users.noreply.github.com>
2021-06-03 11:33:10 +0530

    solution

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,28 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc_pi(int n){
+  int p1=0;
+  int p2=0;
+  float x,y,z;
+  int i=0;
+  while(i<n-1){
+    x=frandom();
+    y=frandom();
+    
+    z=x*x+y*y ;
+    if(z<=1){
+      p1+=1;
+      p2+=1;}
+    else{
+      p2+=1;
+    i++;
+    }
+  }
+  
+  float pi=4.0*p1/p2;
+  return pi;
+}
 
 float frandom() {
   long int q = random();
